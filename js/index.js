807e00613ee847f4381f5a2e1ac15a986009f13d
SH_SOHEL <sujauddoulasohel352@gmail.com>
2024-02-19 23:01:22 +0600

    alhamdulilah compalet website

--- js/index.js
+++ js/index.js
@@ -1,21 +1,30 @@
 const seats = document.querySelectorAll('.kbd');
-let bookingSeat= 1;
+let bookingSeat= 0;
 let totalPrice = 0;
+
 for (let seat of seats) {
     seat.addEventListener("click", function () {
+        if( bookingSeat >= 4){
+            alert("booking seat compalet")
+            return;
+        }
     /**---------seat background-------------**/
-    seat.classList.add("bg-green-500");
-    seat.classList.remove('hov')
+        seat.classList.add("bg-green-500");
+        seat.classList.remove('hov')
         const seatName = seat.innerText;
         const price = parseInt(document.querySelector("#ticket-price").innerText);
         totalPrice = totalPrice + price;
         document.getElementById("Total-Aumont").innerText = totalPrice;
+        
+    const ticketTitleContainer = document.getElementById("seat-title-container");
+    const grandTotal = document.getElementById('Grand-Total');
+    const seatCount = document.getElementById('seat-count');
+    const availableSet = document.getElementById('available-set');
 
-        const ticketTitleContainer = document.getElementById("seat-title-container");
-        const grandTotal = document.getElementById('Grand-Total');
         grandTotal.innerText = totalPrice;
         const addToCardContent = document.createElement('div');
-        addToCardContent.classList.add("text-xl","roboto", "mt-8", "flex", "justify-between", "mx-8", "mb-4");
+        addToCardContent.classList.add("text-xl", "roboto", "mt-8", "flex", "justify-between", "mx-8", "mb-4");
+     
         const h3 = document.createElement('h3');
         h3.innerText = seatName;
         h3.classList.add("text-xl");
@@ -29,27 +38,24 @@ for (let seat of seats) {
         h2.classList.add('text-2xl');
         addToCardContent.appendChild(h2);
         ticketTitleContainer.appendChild(addToCardContent);
-
-        const seatCount = document.getElementById('seat-count');
+        bookingSeat++;
         seatCount.innerText = parseInt(bookingSeat);
-        bookingSeat++
-
-        const availableSet = document.getElementById('available-set');
-       const currentAvailableSetNumber = availableSet.innerText;
-       const currentAvailableSet = parseInt(currentAvailableSetNumber);
-       const newAvailableSet = currentAvailableSet - 1;
-       availableSet.innerText = newAvailableSet 
-       
+        const currentAvailableSet = parseInt(availableSet.innerText);
+        const newAvailableSet = currentAvailableSet - 1;
+        availableSet.innerText = newAvailableSet;
+        
     });
-}
+    
+
+};
 
 const couponBtn = document.getElementById('coupon-apply');
     couponBtn.addEventListener("click",function(){
+        
         const couponInput = document.getElementById('coupon-input').value;
         console.log(couponInput)
         const couponCod = couponInput.split(" ").join("").toUpperCase();
         console.log(couponCod);
-
         if(couponCod === 'COUPLE20'){
             const discount = totalPrice*0.2;
             const discountAmount = document.getElementById("discount-amount");
@@ -66,7 +72,6 @@ const couponBtn = document.getElementById('coupon-apply');
            discountSection.classList.add('hidden');
            const grandTotal = document.getElementById('Grand-Total');
            grandTotal.innerText -= discount;
-
         }else if (couponCod ==='NEW15'){
         const discount = totalPrice*0.15;
         const discountAmount = document.getElementById("discount-amount");
@@ -82,13 +87,22 @@ const couponBtn = document.getElementById('coupon-apply');
        discountSection.classList.add('hidden');
        const grandTotal = document.getElementById('Grand-Total');
        grandTotal.innerText -= discount;
-}
-else{
-    alert('invalid coupont')
-}
-
-
+    }
+    else{
+     alert('invalid coupon')
+   }
+   
     
+   
+   
+   
+});
 
-     });
+
+
+
+        
+
+
+
 
